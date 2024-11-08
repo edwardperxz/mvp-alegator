95389d98e4d478b31eb0f2083ef05164084e147d
edwardperxz <edwardpereztapia@gmail.com>
2024-11-08 00:42:56 -0500

    update main: create Login  and Footer

--- mvp-alegator/src/App.tsx
+++ mvp-alegator/src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import Sidebar from './components/Sidebar';
 import SetPin from './pages/SetPin';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './pages/Login';
 
 const App: React.FC = () => {
   return (
-    <div className="flex h-screen">
-      <Sidebar />
+    <div className="flex flex-col h-screen">
+      <div className="flex-grow">
+      </div>
+      <Login />
     </div>
   );
 };
