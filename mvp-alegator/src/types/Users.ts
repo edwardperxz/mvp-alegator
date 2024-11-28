5fea8773de4d292ccdeccf1eb218648920dacb38
Luislev <luis.leiva@utp.ac.pa>
2024-11-27 23:36:45 -0500

    feat: add initial types class files

---
+++ mvp-alegator/src/types/Users.ts
@@ -0,0 +1,16 @@
+export class UserData {
+    id?: string;
+    username?: string;
+    short_id?: string;
+    email?: string;
+    first_name?: string;
+    last_name?: string;
+    is_active?: boolean;
+    is_admin?: boolean;
+    created_at?: string;
+}
+
+export class UserDashboard {
+    username?: string;
+    short_id?: string;
+}
