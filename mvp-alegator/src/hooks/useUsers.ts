b6f60fd5dd205e64da7691a030d39d6dd64c2d23
Luislev <luis.leiva@utp.ac.pa>
2024-11-27 23:35:17 -0500

    feat: add initial hooks for supabase data fetch

---
+++ mvp-alegator/src/hooks/useUsers.ts
@@ -0,0 +1,60 @@
+import { supabase } from '../supabaseClient';
+import type { UserData, UserDashboard } from '../types/Users';
+
+
+export default () => {
+    return {
+        fetchUsernameAndShortId,
+        fetchAllUsers,
+        fetchUserbyShortId,
+    };
+}
+
+async function fetchUsernameAndShortId(userId: string): Promise<UserDashboard> {
+    const { data, error } = await supabase
+        .from('users')
+        .select('username, short_id')
+        .eq('id', userId)
+        .single();
+
+    if (error) {
+        console.error('Error al obtener los datos del usuario:', error.message);
+        throw error;
+    }
+
+    if (!data) {
+        throw new Error('No se encontraron datos del usuario');
+    }
+    return data as UserDashboard;
+}
+
+async function fetchAllUsers(): Promise<UserData[]> {
+    const { data, error } = await supabase
+    .from('users')
+    .select('*');
+
+    if (error) {
+        console.error('Error al obtener todos los usuarios:', error.message);
+        throw error;
+    }
+
+    return data as UserData[];
+}
+
+async function fetchUserbyShortId(shortId: string): Promise<UserData> {
+    const { data, error } = await supabase
+        .from('users')
+        .select('*')
+        .eq('short_id', shortId)
+        .single();
+
+    if (error) {
+        console.error('Error al obtener los datos del usuario:', error.message);
+        throw error;
+    }
+
+    if (!data) {
+        throw new Error('No se encontraron datos del usuario');
+    }
+    return data as UserData;
+}
